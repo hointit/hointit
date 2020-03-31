46bdd33ac2d6dceca22002f975125a3a47b3fe2f
Tien Hoi Nguyen <hoint@techvn.mti.co.jp>
2020-03-31 13:43:05 +0700

    Add project files.

---
+++ ModuleU/ModuleUModule.cs
@@ -0,0 +1,11 @@
+﻿using ModuleA.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace ModuleU
+{
+    public class ModuleUModule:IModule
+    {
+    }
+}
---
+++ PrismRole/Bootstrapper.cs
@@ -0,0 +1,34 @@
+﻿using Unity;
+using Prism.Unity;
+using Prism.Modularity;
+using System.Windows;
+using Unity.Lifetime;
+using PrismRole.Views;
+using ModuleA;
+
+namespace PrismRole
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+            ModuleCatalog.AddModule<ModuleAModule>();
+
+            //Container.RegisterType<IModuleInitializer, Core.RoleBasedModuleInitializer>(
+            //    new ContainerControlledLifetimeManager());
+
+        }
+    }
+}
